aa724159f1dfe85940b40083409ce3c639fa2cbe
flaviusgavriluta <flavius.gavriluta@gmail.com>
2024-01-13 00:56:56 +0200

    solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Main {
+    public static int getCount(String str) {
+        return (int) str.chars().filter(c -> "aeiouAEIOU".indexOf(c) > 0).count();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input = "HellO world!";
+        int vowelCount = getCount(input);
+        System.out.println("Number of vowels: " + vowelCount);
     }
 }
